68e0f39146a2d6fb6e12e46bf403421702e81b68
Student <w32191@gmail.com>
2021-12-23 12:59:20 +0800

    abc

--- src/com/samwang/hw1/Equation.java
+++ src/com/samwang/hw1/Equation.java
@@ -6,12 +6,14 @@ public class Equation {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("ax^2 + bx + c = 0");
+
 		int a, b, c;
-		System.out.println("a:");
+		System.out.println("請輸入a:");
 		a = scanner.nextInt();
-		System.out.println("b:");
+		System.out.println("請輸入b:");
 		b = scanner.nextInt();
-		System.out.println("c:");
+		System.out.println("請輸入c:");
 		c = scanner.nextInt();
 
 		double condition = b * b - 4 * a * c;
@@ -19,10 +21,10 @@ public class Equation {
 			double result1, result2;
 			result1 = (-b + Math.sqrt(condition)) / 2 * a;
 			result2 = (-b - Math.sqrt(condition)) / 2 * a;
-			System.out.println("實根為" + result1 + " & " + result2);
+			System.out.println("x為" + result1 + " & " + result2);
 		} else if (condition == 0) {
 			double result = -b / 2 * a;
-			System.out.println("實根為" + result);
+			System.out.println("x為" + result);
 		} else {
 			System.out.println("沒有實根");
 		}
