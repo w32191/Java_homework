789cb5109d777ad598ac96f161372ecc90e981f5
Student <w32191@gmail.com>
2021-12-23 12:48:17 +0800

    condition edit

--- src/com/samwang/hw1/Electricity.java
+++ src/com/samwang/hw1/Electricity.java
@@ -12,19 +12,19 @@ public class Electricity {
 
     System.out.println("請輸入使用度數:");
     int electricity = scanner.nextInt(); // 度數
-    double price = 0; // 電費
+    double price; // 電費
 
     switch (isCommercial) {
       case "A": // 非營業用
         if (electricity <= 120) {
           price = electricity * 1.63;
-        } else if (electricity >= 121 && electricity <= 330) {
+        } else if (electricity <= 330) {
           price = 120 * 1.63 + (electricity - 120) * 2.1;
-        } else if (electricity >= 331 && electricity <= 500) {
+        } else if (electricity <= 500) {
           price = 120 * 1.63 + 210 * 2.1 + (electricity - 330) * 2.89;
-        } else if (electricity >= 501 && electricity <= 700) {
+        } else if (electricity <= 700) {
           price = 120 * 1.63 + 210 * 2.1 + 170 * 2.89 + (electricity - 500) * 3.94;
-        } else if (electricity >= 701 && electricity <= 1000) {
+        } else if (electricity <= 1000) {
           price = 120 * 1.63 + 210 * 2.1 + 170 * 2.89 + 200 * 3.94 + (electricity - 700) * 4.6;
         } else { // 1001以上
           price = 120 * 1.63 + 210 * 2.1 + 170 * 2.89 + 200 * 3.94 + 300 * 4.6
@@ -37,9 +37,9 @@ public class Electricity {
       case "B": // 營業用
         if (electricity <= 330) {
           price = electricity * 2.12;
-        } else if (electricity >= 331 && electricity <= 700) {
+        } else if (electricity <= 700) {
           price = 330 * 2.12 + (electricity - 330) * 2.91;
-        } else if (electricity >= 701 && electricity <= 1500) {
+        } else if (electricity <= 1500) {
           price = 330 * 2.12 + 370 * 2.91 + (electricity - 700) * 3.44;
         } else { // 1500以上
           price = 330 * 2.12 + 370 * 2.91 + 800 * 3.44 + (electricity - 1500) * 5.05;
