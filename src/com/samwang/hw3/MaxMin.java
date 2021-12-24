df1bbf0f58148909de62706a28270437c8ac0d2c
Student <w32191@gmail.com>
2021-12-24 15:38:53 +0800

    hw3 01OK

--- src/com/samwang/hw3/MaxMin.java
+++ src/com/samwang/hw3/MaxMin.java
@@ -8,23 +8,11 @@ public class MaxMin {
   public static void main(String[] args) {
     //事先將10個數字置於一維陣列中，分別找出10個數字中最大的值和最小的值。
     int[] array = {53, 12, 8, 40, 75, 14, 54, 46, 42, 45};
-    System.out.println(Arrays.toString(array));
-//    Random rand = new Random();
 
-//    for (int i = 0; i < array.length; i++) {
-//      array[i] = rand.nextInt(100);
-//    }
-//    for (int i : array
-//    ) {
-//      System.out.print(i + " ");
-//    }
-//    System.out.println();
+    int max = array[0];//設第一個最大，依序比較
 
-    int max = array[0];
-    for (int i = 1; i < array.length; i++) {
-      if (max > array[i]) {
-        continue;
-      } else {
+    for (int i = 1; i < array.length; i++) {  //從index=1開始
+      if (max < array[i]) {
         max = array[i];
       }
     }
