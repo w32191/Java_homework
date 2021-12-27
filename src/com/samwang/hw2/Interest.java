de13a815945b13010a436b2663304355047d061f
王柏翔 <w32191@gmail.com>
2021-12-27 13:43:23 +0800

    Interest

--- src/com/samwang/hw2/Interest.java
+++ src/com/samwang/hw2/Interest.java
@@ -3,12 +3,32 @@ package com.samwang.hw2;
 public class Interest {
 
   public static void main(String[] args) {
-    boolean isOverMoney = true;
-    int n = 1;
+
+//    int n = 1;
+//    double A, B;
+//    while (true) {
+//      A = 100000 * (1 + 0.1 * n);
+//      B = 100000 * Math.pow((1 + 0.05), n);
+//      if (B > A) {
+//        System.out.println(n + "年");
+//        break;
+//      } else {
+//        n++;
+//      }
+//    }
+    int n = 1, P = 100000;
     double A, B;
+
     while (true) {
-      A = 100000 * (1 + 0.1 * n);
-      B = 100000 * Math.pow((1 + 0.05), n);
+      double rate = 1.0;
+
+      A = P * (1 + 0.1 * n);
+
+      for (int i = 1; i <= n; i++) {
+        rate = rate * 1.05;
+      }
+      B = P * rate;
+
       if (B > A) {
         System.out.println(n + "年");
         break;
@@ -16,6 +36,5 @@ public class Interest {
         n++;
       }
     }
-
   }
 }
