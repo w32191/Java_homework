2b5238a92dc23a0166d317c275ef923830e89e0c
王柏翔 <w32191@gmail.com>
2021-12-27 13:49:41 +0800

    Merge remote-tracking branch 'origin/master'

--- src/com/samwang/hw2/Interest.java
+++ src/com/samwang/hw2/Interest.java
@@ -3,19 +3,29 @@ package com.samwang.hw2;
 public class Interest {
 
   public static void main(String[] args) {
-    boolean isOverMoney = true;
-    int n = 1;
+
+    int n = 1, P = 100000;
     double A, B;
+
     while (true) {
-      A = 100000 * (1 + 0.1 * n);
-      B = 100000 * Math.pow((1 + 0.05), n);
+      //A
+      A = P * (1 + 0.1 * n);
+
+      //B
+      double rate = 1.0;
+      for (int i = 1; i <= n; i++) {
+        rate *= 1.05;
+      }
+      B = P * rate;
+
       if (B > A) {
         System.out.println(n + "年");
         break;
       } else {
         n++;
       }
-    }
+    } //end of while()
+
 
   }
 }
