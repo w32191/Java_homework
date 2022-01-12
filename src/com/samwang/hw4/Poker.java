35cdadd0fdbee2a3fe6c950e04c9bedfbb12e063
samwang <w32191@gmail.com>
2022-01-13 00:07:33 +0800

    hw4 IDGenerator ＯＫ

--- src/com/samwang/hw4/Poker.java
+++ src/com/samwang/hw4/Poker.java
@@ -6,20 +6,12 @@ import java.util.List;
 
 public class Poker {
 
-  int[][] card = {
-      {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12},
-      {13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25},
-      {26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38},
-      {39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
-  };
-  // 花色i/13，數字i%13+1
-  private int randomCard;
-
   List<Integer> list = new ArrayList<>();
 
   public void randomCard() {
     do {
-      randomCard = (int) (Math.random() * 52);
+      // 花色i/13，數字i%13+1
+      int randomCard = (int) (Math.random() * 52);
       if (!list.contains(randomCard)) {
         list.add(randomCard);
       }
